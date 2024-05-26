6cc738e34b42af9d609af3283ac8e6d56a9163bb
lemeote <dorastiku@gmail.com>
2024-05-25 19:51:52 -0500

    Add input handling for UI to manage configuration and exit

--- src/generator.js
+++ src/generator.js
@@ -3,6 +3,11 @@
 const DefaultBox = require("../Components/DefaultBox");
 
 require("dotenv").config();
+const React = require("react");
+const { useState, useEffect } = require("react");
+const fs = requrie("fs");
+const { Text, Box, useApp, useInput, Newline, useStdin } = require("ink");
+const { default: SelectInput } = require("ink-select-input");
 
 const tradingNodes = [
 	{ label: "pingpong", value: "pingpong" },
@@ -10,7 +15,59 @@ const tradingNodes = [
 ];
 
 const App = (props) => {
-    return <DefaultBox></DefaultBox>
+	const [network, setNetwork] = useState(props.network || "");
+	const [rpcURL, setRpcURL] = useState(props.rpc);
+	const [rpc, setRpc] = useState([]);
+	const [isRpcsSet, setIsRpcsSet] = useState(false);
+	const [tradingMode, setTradingMode] = useState("");
+	const [tokens, setTokens] = useState([]);
+	const [tokenA, setTokenA] = useState({});
+	const [tokenB, setTokenB] = useState({});
+	const [tokenAisSet, setTokenAisSet] = useState(false);
+	const [tokenBisSet, setTokenBisSet] = useState(false);
+	const [tradingEnabled, setTradingEnabled] = useState(undefined);
+	const [tradeSize, setTradeSize] = useState("");
+	const [minPercProfit, setMinPercProfit] = useState("1");
+	const [isMinPercProfitSet, setIsMinPercProfitSet] = useState(false);
+	const [minInterval, setMinInterval] = useState("30");
+	const [storeFailedTxInHistory, setStoreFailedTxInHistory] = useState(true);
+	const [readyToStart, setReadyToStart] = useState(false);
+
+	const { exit } = useApp();
+
+	useInput((input, key) => {
+		if (key.escape) exit();
+		if (key.tab && !isRpcsSet) {
+			setRpc([process.env.DEFAULT_RPC]);
+			setIsRpcsSet(true);
+		}
+		if (readyToStart && key.return) {
+			const config = {
+				tokenA,
+				tokenB,
+				tradingMode,
+				tradeSize: parseFloat(tradeSize),
+				network,
+				rpc,
+				minPercProfit: parseFloat(minPercProfit),
+				minInterval: parseInt(minInterval),
+				tradingEnabled,
+				storeFailedTxInHistory,
+				ui: {
+					defaultColor: "cyan",
+				},
+			};
+
+			// save config to config.json file
+			fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+
+			// save tokenst to tokens.json file
+			fs.writeFileSync("./temp/tokens.json", JSON.stringify(tokens, null, 2));
+			exit();
+		}
+	});
+
+	return <DefaultBox></DefaultBox>;
 };
 
 module.exports = App;
