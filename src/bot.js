7a02093628b75c0c176cf3cdc7b3ef8f1a8b5dc0
lemeote <dorastiku@gmail.com>
2024-05-25 20:53:04 -0500

    Implement ping-pong trading mode for iterative swaps

--- src/bot.js
+++ src/bot.js
@@ -181,3 +181,205 @@ const successSwapHandler = (tx, tradeEntry, tokenA, tokenB) => {
 		cache.firstSwapInQueue = false;
 	}
 };
+
+const pingpongMode = async (jupiter, tokenA, tokenB) => {
+	cache.iteration++;
+	const date = new Date();
+	const i = cache.iteration;
+	cache.queue[i] = -1;
+	if (cache.firstSwap) cache.firstSwapInQueue = true;
+	try {
+		// calculate & update iteration per minute
+		const iterationTimer =
+			(performance.now() - cache.iterationPerMinute.start) / 1000;
+
+		if (iterationTimer >= 60) {
+			cache.iterationPerMinute.value = Number(
+				cache.iterationPerMinute.counter.toFixed()
+			);
+			cache.iterationPerMinute.start = performance.now();
+			cache.iterationPerMinute.counter = 0;
+		} else cache.iterationPerMinute.counter++;
+
+		// Calculate amount that will be used for trade
+		const amountToTrade = cache.firstSwap
+			? cache.initialBalance.tokenA
+			: cache.currentBalance[cache.sideBuy ? "tokenA" : "tokenB"];
+		const baseAmount = cache.lastBalance[cache.sideBuy ? "tokenB" : "tokenA"];
+
+		// default slippage
+		const slippage = 1;
+
+		// set input / output token
+		const inputToken = cache.sideBuy ? tokenA : tokenB;
+		const outputToken = cache.sideBuy ? tokenB : tokenA;
+
+		// check current routes
+		const performanceOfRouteCompStart = performance.now();
+		const routes = await jupiter.computeRoutes({
+			inputMint: new PublicKey(inputToken.address),
+			outputMint: new PublicKey(outputToken.address),
+			inputAmount: amountToTrade,
+			slippage,
+			forceFeech: true,
+		});
+
+		// count available routes
+		cache.availableRoutes[cache.sideBuy ? "buy" : "sell"] =
+			routes.routesInfos.length;
+
+		// update status as OK
+		cache.queue[i] = 0;
+
+		const performanceOfRouteComp =
+			performance.now() - performanceOfRouteCompStart;
+
+		// choose first route
+		const route = await routes.routesInfos[0];
+
+		// update slippage with "profit or kill" slippage
+		const profitOrKillSlippage = cache.firstSwap
+			? route.outAmountWithSlippage
+			: cache.lastBalance[cache.sideBuy ? "tokenB" : "tokenA"];
+
+		route.outAmountWithSlippage = profitOrKillSlippage;
+
+		// calculate profitability
+
+		let simulatedProfit = cache.firstSwap
+			? 0
+			: calculateProfit(baseAmount, await route.outAmount);
+
+		// store max profit spotted
+		if (
+			simulatedProfit > cache.maxProfitSpotted[cache.sideBuy ? "buy" : "sell"]
+		) {
+			cache.maxProfitSpotted[cache.sideBuy ? "buy" : "sell"] = simulatedProfit;
+		}
+
+		printToConsole({
+			date,
+			i,
+			performanceOfRouteComp,
+			inputToken,
+			outputToken,
+			tokenA,
+			tokenB,
+			route,
+			simulatedProfit,
+			cache,
+			config,
+		});
+
+		// check profitability and execute tx
+		let tx, performanceOfTx;
+		if (
+			!cache.swappingRightNow &&
+			(cache.firstSwap ||
+				cache.hotkeys.e ||
+				cache.hotkeys.r ||
+				simulatedProfit >= config.minPercProfit)
+		) {
+			// hotkeys
+			if (cache.hotkeys.e) {
+				console.log("[E] PRESSED - EXECUTION FORCED BY USER!");
+				cache.hotkeys.e = false;
+			}
+			if (cache.hotkeys.r) {
+				console.log("[R] PRESSED - REVERT BACK SWAP!");
+			}
+
+			if (cache.tradingEnabled || cache.hotkeys.r) {
+				cache.swappingRightNow = true;
+				// store trade to the history
+				let tradeEntry = {
+					date: date.toLocaleString(),
+					buy: cache.sideBuy,
+					inputToken: inputToken.symbol,
+					outputToken: outputToken.symbol,
+					inAmount: toDecimal(route.inAmount, inputToken.decimals),
+					expectedOutAmount: toDecimal(route.outAmount, outputToken.decimals),
+					expectedProfit: simulatedProfit,
+				};
+
+				// start refreshing status
+				const printTxStatus = setInterval(() => {
+					if (cache.swappingRightNow) {
+						printToConsole({
+							date,
+							i,
+							performanceOfRouteComp,
+							inputToken,
+							outputToken,
+							tokenA,
+							tokenB,
+							route,
+							simulatedProfit,
+							cache,
+							config,
+						});
+					}
+				}, 500);
+
+				[tx, performanceOfTx] = await swap(jupiter, route);
+
+				// stop refreshing status
+				clearInterval(printTxStatus);
+
+				const profit = cache.firstSwap
+					? 0
+					: calculateProfit(
+							cache.currentBalance[cache.sideBuy ? "tokenB" : "tokenA"],
+							tx.outputAmount
+					  );
+
+				tradeEntry = {
+					...tradeEntry,
+					outAmount: tx.outputAmount || 0,
+					profit,
+					performanceOfTx,
+					error: tx.error?.message || null,
+				};
+
+				// handle TX results
+				if (tx.error) failedSwapHandler(tx, tradeEntry, route);
+				else {
+					if (cache.hotkeys.r) {
+						console.log("[R] - REVERT BACK SWAP - SUCCESS!");
+						cache.tradingEnabled = false;
+						console.log("TRADING DISABLED!");
+						cache.hotkeys.r = false;
+					}
+					successSwapHandler(tx, tradeEntry, tokenA, tokenB);
+				}
+			}
+		}
+
+		if (tx) {
+			if (!tx.error) {
+				// change side
+				cache.sideBuy = !cache.sideBuy;
+			}
+			cache.swappingRightNow = false;
+		}
+
+		printToConsole({
+			date,
+			i,
+			performanceOfRouteComp,
+			inputToken,
+			outputToken,
+			tokenA,
+			tokenB,
+			route,
+			simulatedProfit,
+			cache,
+			config,
+		});
+	} catch (error) {
+		cache.queue[i] = 1;
+		console.log(error);
+	} finally {
+		delete cache.queue[i];
+	}
+};
